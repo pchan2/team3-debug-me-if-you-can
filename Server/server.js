bdf1b74f3cedb970523a1cad9ff3954d6669aeec
Leroy Douglas <leroi.douglas@gmail.com>
2020-11-19 04:17:52 +0000

    Update server.js
    Added some comments for middleware calls

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4002;
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.json());                    // body parser
+app.use(express.urlencoded());         // HTML forms parser
 
 app.listen(PORT, _ => console.log(`server is listening on ${PORT}`));
