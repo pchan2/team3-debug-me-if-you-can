bea0e5ab5fa5b6abdef2a3b0107a7321c0688667
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-11-20 14:01:49 +0000

    included pd at start of routes

--- Server/routes/studentPD.js
+++ Server/routes/studentPD.js
@@ -3,32 +3,32 @@ const express = require("express");
 const studentData = require("../json/studentPD.json");
 const studentPD = express.Router();
 
-studentPD.get('/', (request, response) => {
+studentPD.get('/pd', (request, response) => {
     //description of routes
    response.send("type /students for all students, /students/(id) to search for students by id, /students/focus/(score) to search by student's focus score, /students/search?name=(name) to search by student's name")
 });
 
 // gets all students data
-studentPD.get("/students", (request, response) => {
+studentPD.get("/pd/students", (request, response) => {
   response.json(studentData);
 });
 
 //proof of concept can search by score for focus heading
-studentPD.get("/students/focus/:score", (request, response) => {
+studentPD.get("/pd/students/focus/:score", (request, response) => {
   const { score } = request.params;
   const focusByScore = studentData.filter((student) => student.Focus == score);
   response.json(focusByScore);
 });
 
 //gets student by name, for testing put numbers as name
-studentPD.get("/students/search", (request, response) => {
+studentPD.get("/pd/students/search", (request, response) => {
   const searchName = request.query.name;
 
   response.json(studentData.filter((student) => student.name.includes(searchName)));
 });
 
 // gets students data by ID
-studentPD.get("/students/:id", (request, response) => {
+studentPD.get("/pd/students/:id", (request, response) => {
   const { id } = request.params;
   const foundStudent = studentData.find(student => student.student_id == id);
 
